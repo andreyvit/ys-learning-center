dd7e2993ffc5874d9bdae652f265f7616e938601
Andrey Tarantsov <andreyvit@gmail.com>
2009-05-21 18:59:37 +0700

    Added webapp HTML/CSS template.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
+import os
 import wsgiref.handlers
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 
-class MainHandler(webapp.RequestHandler):
+template_path = os.path.join(os.path.dirname(__file__), 'templates')
+
+class BaseHandler(webapp.RequestHandler):
+  
+  def __init__(self):
+    self.data = {}
+
+  def render(self, *path_components):
+    self.response.out.write(template.render(os.path.join(template_path, *path_components), self.data))
+  
+class MainHandler(BaseHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    self.render('index.html')
 
 url_mapping = [
   ('/', MainHandler),
